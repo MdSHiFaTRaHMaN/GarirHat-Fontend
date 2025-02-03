e8eec7ccebf0bf0c6a92323ae87481b0e5855718
ShifatRahman420 <copyremc4@gmail.com>
2025-02-03 19:45:52 +0600

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import Router from './Routes/Router'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import Router from './routes/Router';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
